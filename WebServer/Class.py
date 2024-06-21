e118adb373068d797abab96d830670b810ee3f22
Mohammad Heydari <HMohammad2520@Gmail.com>
2024-06-21 20:07:26 +0330

    Removed unused imports , some syntax improvments

--- WebServer/Class.py
+++ WebServer/Class.py
@@ -1,30 +1,27 @@
-from flask                import Flask, redirect, request, url_for
-from flask_login          import LoginManager , current_user
-from flask_htmlmin        import HTMLMIN
-from Global.Constant      import WEBSERVER_DEBUG , WEBSERVER_FLASK_DEBUG , WEBSERVER_LOG , WEBSERVER_SECRET_KEY , WEBSERVER_MINIFY_HTML , WEBSERVER_TIDYFY_HTML , WEBSERVER_MAX_CONTENT_LENGTH
-from Global.Constant      import NAME , ENCODE , LANGUAGE
-from Global.Decorator     import Do_Log
-from Global.Class.Logger  import Logger
+from flask import Flask
+from flask_login import LoginManager
+from flask_htmlmin import HTMLMIN
+from Global.Constant import WEBSERVER_DEBUG , WEBSERVER_FLASK_DEBUG , WEBSERVER_LOG , WEBSERVER_SECRET_KEY , WEBSERVER_MINIFY_HTML
+from Global.Decorator import Do_Log
+from Global.Class.Logger import Logger
 from Global.Class.Network import IPv4 , Port
-from SQLManager           import SQLManager
 
 class FlaskWebServer :
     def __init__(self , Host : IPv4 , Port : Port) -> None:
-        self.Logger = Logger('Web Server' , WEBSERVER_LOG , WEBSERVER_DEBUG)
+        self.Logger = Logger('Web Server', WEBSERVER_LOG, WEBSERVER_DEBUG)
         self.Host = Host
         self.Port = Port
 
         self.App  = Flask(__name__ , static_folder = "Static" , template_folder = "Templates")
         Login_Manager = LoginManager()
         Login_Manager.init_app(self.App)
-        self.App.config['MINIFY_HTML']        = WEBSERVER_MINIFY_HTML
-        self.App.config['SECRET_KEY']         = WEBSERVER_SECRET_KEY
-        self.App.config['MAX_CONTENT_LENGTH'] = WEBSERVER_MAX_CONTENT_LENGTH
+        self.App.config['MINIFY_HTML'] = WEBSERVER_MINIFY_HTML
+        self.App.config['SECRET_KEY'] = WEBSERVER_SECRET_KEY
 
         if self.App.config['MINIFY_HTML'] : self.HTMLmin = HTMLMIN(self.App)
-    
+
     @Do_Log('Registering Blueprints...','Done!')
-    def Init_Blueprints(self , Blueprints : list) -> None :
+    def Init_Blueprints(self , Blueprints : list) -> None:
         if self.Valid :
             for Item in Blueprints :
                 self.Logger(f'Registering : {Item.__Prefix__}')
@@ -41,6 +38,3 @@ class FlaskWebServer :
 
     def __str__(self) -> str:
         return f'< FlaskApp {self.App.name} >'
-
-    def __bool__(self) -> bool :
-        return self.Valid
