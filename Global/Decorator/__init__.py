883983c9b091a6309ddf08fd78ce0c9f6acadf50
Mohammad Heydari <HMohammad2520@Gmail.com>
2024-06-30 22:06:18 +0330

    Added New Decorator for bool return type functions

--- Global/Decorator/__init__.py
+++ Global/Decorator/__init__.py
@@ -2,10 +2,17 @@ import tracemalloc
 from functools import wraps
 from time import perf_counter
 
+def Return_False_On_Exception(Function) :
+    @wraps(Function)
+    def Wrapper(*Args, **Kwargs):
+        try : return Function(*Args, **Kwargs)
+        except : return False
+    return Wrapper
+
 def Do_Log (Before = '' , After = '') :
     def Inner (Function) :
         @wraps(Function)
-        def Wrapper(*Args , **Kwargs):
+        def Wrapper(*Args, **Kwargs):
             if Before : Args[0].Logger(Before)
             Result = Function(*Args , **Kwargs)
             if After  : Args[0].Logger(After)
@@ -15,14 +22,14 @@ def Do_Log (Before = '' , After = '') :
 
 def Do_Performance (Function) :
     @wraps(Function)
-    def Wrapper(*Args , **Kwargs):
+    def Wrapper(*Args, **Kwargs):
         tracemalloc.start()
         Start_Time = perf_counter()
-        Function(*Args , **Kwargs)
-        Current , Peak = tracemalloc.get_traced_memory()
+        Function(*Args, **Kwargs)
+        Current, Peak = tracemalloc.get_traced_memory()
         End_Time = perf_counter()
         print(f'Performance : [StartTime:{Start_Time},MemoryUsage:{Current},Peak:{Peak},EndTime{End_Time}]')
         tracemalloc.stop()
-        Result = Function(*Args , **Kwargs)
+        Result = Function(*Args, **Kwargs)
         return Result
     return Wrapper
