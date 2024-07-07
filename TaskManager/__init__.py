6ae130c2e7c9e38969ef8560248c936262a7d9fe
Mohammad Heydari <HMohammad2520@Gmail.com>
2024-07-07 22:12:32 +0330

    Removed WhiteSpace

--- TaskManager/__init__.py
+++ TaskManager/__init__.py
@@ -1,6 +1,6 @@
 from Global import Main_Config
 from .Class import SimpleTaskManager
 
-TaskManager = SimpleTaskManager('Task Manager',
+TaskManager = SimpleTaskManager('TaskManager',
     Check_Interval = Main_Config.Get('TASKMANAGER','chck_interval'),
     )
