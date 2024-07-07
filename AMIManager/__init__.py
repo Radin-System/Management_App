60fab9426a94bf7e03925828f06aae2cad8e2ab6
Mohammad Heydari <Hmohammad2520@gmail.com>
2024-07-07 09:56:25 +0330

    Syntax Fixes

--- AMIManager/__init__.py
+++ AMIManager/__init__.py
@@ -1,11 +1,11 @@
-from .Class          import AsteriskAMIManager
+from .Class import AsteriskAMIManager
 from Global import Main_Config
 
 AMIManager = AsteriskAMIManager('AMIManager',
-    Host     = Main_Config.Get('AMIMANAGER','host'),
-    Port     = Main_Config.Get('AMIMANAGER','port'),
-    Username = Main_Config.Get('AMIMANAGER','username'),
-    Password = Main_Config.Get('AMIMANAGER','password'),
+    Host            = Main_Config.Get('AMIMANAGER','host'),
+    Port            = Main_Config.Get('AMIMANAGER','port'),
+    Username        = Main_Config.Get('AMIMANAGER','username'),
+    Password        = Main_Config.Get('AMIMANAGER','password'),
     Event_Whitelist = Main_Config.Get('AMIMANAGER','event_whitelist_csv'),
-    Timeout  =  Main_Config.Get('AMIMANAGER','timeout'),
+    Timeout         = Main_Config.Get('AMIMANAGER','timeout'),
     )
