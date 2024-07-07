505753b9011c49f62a26eb9e6f9110c336d95235
Mohammad Heydari <HMohammad2520@Gmail.com>
2024-07-07 22:10:00 +0330

    Fixed Running order and false warning

--- Server.py
+++ Server.py
@@ -1,6 +1,6 @@
 if __name__ == '__main__' :
-    from Global import Main_Logger,Main_Config
-    
+    from Global import Main_Config as _
+    from Global import Main_Logger
+
     Main_Logger('Configs and Logging system Loaded')
     Main_Logger('App Works Fine')
-    Main_Logger(Main_Config.__dict__)
