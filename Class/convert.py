ae1ef2e7682f92ae4b3370cc1256c4f9a47cfff3
Mohammad Heydari <HMohammad2520@Gmail.com>
2024-07-12 00:46:20 +0330

    Removed Unused Convert Methods

--- Class/convert.py
+++ Class/convert.py
@@ -1,20 +1,5 @@
-from . import Validate
-from Class.constants import CIDR_MAP, MASK_MAP
-
 class Convert :
 
     @staticmethod
     def CSVToList(CSV:str) -> list :
         return [Item.strip() for Item in CSV.split(',')]
-
-    @staticmethod
-    def MaskToCIDR(Mask : str) -> str :
-        if Validate.Mask(Mask) :
-           return CIDR_MAP.get(Mask , '')
-        return ''
-
-    @staticmethod
-    def CIDRToMask(CIDR : str) -> str :
-        if Validate.CIDR(CIDR) :
-            return MASK_MAP.get(CIDR , '')
-        return ''
