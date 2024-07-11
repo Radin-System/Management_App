47420c9dbdcc0e89532f36483cf315ece35c6d28
Mohammad Heydari <HMohammad2520@Gmail.com>
2024-07-11 18:19:20 +0330

    Moved __init__ contents in Main Server

--- Server.py
+++ Server.py
@@ -1,6 +1,38 @@
 if __name__ == '__main__' :
-    from Global import Main_Config as _
+    from Global import Main_Config
     from Global import Main_Logger
 
-    Main_Logger('Configs and Logging system Loaded')
-    Main_Logger('App Works Fine')
+    from TaskManager import SimpleTaskManager
+    
+    from SQLManager         import SQLAlchemyManager
+    from SQLManager.Models  import Base,Models
+
+    from AMIManager import AsteriskAMIManager
+
+    TaskManager = SimpleTaskManager('TaskManager',
+        Check_Interval = Main_Config.Get('TASKMANAGER','chck_interval'),
+        )
+    
+    SQLManager = SQLAlchemyManager('SQLManager',
+        Host        = Main_Config.Get('SQLMANAGER','host'),
+        Port        = Main_Config.Get('SQLMANAGER','port'), 
+        Username    = Main_Config.Get('SQLMANAGER','username'), 
+        Password    = Main_Config.Get('SQLMANAGER','password'),
+        DataBase    = Main_Config.Get('SQLMANAGER','database'),
+        Mode        = Main_Config.Get('SQLMANAGER','mode'),
+        SQLite_Path = Main_Config.Get('SQLMANAGER','sqlite_path'),
+        Verbose     = Main_Config.Get('SQLMANAGER','verbose'),
+        )
+    SQLManager.Init_Base(Base = Base)
+    SQLManager.Create_Engine()
+    SQLManager.Init_Models(Models = Models)
+
+    AMIManager = AsteriskAMIManager('AMIManager',
+        Host            = Main_Config.Get('AMIMANAGER','host'),
+        Port            = Main_Config.Get('AMIMANAGER','port'),
+        Username        = Main_Config.Get('AMIMANAGER','username'),
+        Password        = Main_Config.Get('AMIMANAGER','password'),
+        Event_Whitelist = Main_Config.Get('AMIMANAGER','event_whitelist_csv'),
+        Timeout         = Main_Config.Get('AMIMANAGER','timeout'),
+        Max_ActionID    = Main_Config.Get('AMIMANAGER','max_action_id'),
+        )
