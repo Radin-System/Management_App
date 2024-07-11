c3287581021ec9a4b5a35c552be5f34e02f2a20e
Mohammad Heydari <HMohammad2520@Gmail.com>
2024-07-11 23:43:17 +0330

    Added nore test cases

--- test.py
+++ test.py
@@ -2,14 +2,29 @@ from Typing import Username,Password,Domain,IPv4,Email
 
 if __name__ == '__main__' :
     test_usernames = [
-        'm.heydari','_admin','Alireza4251',
+        'm.heydari',
+        '_admin',
+        'Alireza4251',
+        'Mamad Heydari',
+        'Mohamm@d',
+        'Am!r"',
+        'a'
     ]
 
     test_passwords = [
-        'HDie@3419','asd','Admin','asd@123',
+        'HDie@3419',
+        'asd',
+        'Admin',
+        'asd@123',
     ]
     test_domains = [
-        'rsto.ir','reza','zabbix.sdiqoifqwofid.qofnoqudf.qfenfoqjenfdqfn.qpdfoqdf','-.asd..',
+        'rsto.ir',
+        'reza',
+        'zabbix.sdiqoifqasdasdasdqwerqwrqdasdasdqwerasdqwfgqegqrgqdfwofid.qofnoqudfdqwfqgerqrg.qfenfoqjenfdqfnqefqegqsafas.qpdfoqdf',
+        '-ali.asd',
+        'mamad..ali',
+        '7ho.st'
+        'Google.com',
     ]
 
     test_ipv4s = [
@@ -17,6 +32,10 @@ if __name__ == '__main__' :
         '192.168.1.1',
         '256.143.1.4',
         'Admin',
+        '172.24.10.1/32',
+        '172.42.17/255.255.252.0',
+        '172.20.30.2/0.255.255.255',
+
     ]
 
     test_emails = [
@@ -27,23 +46,38 @@ if __name__ == '__main__' :
         'Admin',
     ]
 
+    ats = 10*'#'
+
+    print(ats)
+    print(ats)
+
     for User in test_usernames :
         try : print(f'Valid Username : {Username(User)}')
         except ValueError as e : print(f'Invalid Username : {User} : {e}')
-    
+
+    print(ats)
+
     for Pass in test_passwords :
         try : print(f'Valid Password : {Password(Pass)}')
         except ValueError as e : print(f'Invalid Password : {Pass} : {e}')
     
+    print(ats)
+
     for Dom in test_domains :
         try : print(f'Valid Domain : {Domain(Dom)}')
         except ValueError as e : print(f'Invalid Domain : {Dom} : {e}')
     
+    print(ats)
+
     for ip in test_ipv4s :
         try : print(f'Valid IPv4 : {IPv4(ip)}')
         except ValueError as e : print(f'Invalid IPv4 : {ip} : {e}')
     
+    print(ats)
+
     for email in test_emails :
         try : print(f'Valid Email : {Email(email)}')
         except ValueError as e : print(f'Invalid Email : {email} : {e}')
     
+    print(ats)
+    print(ats)
