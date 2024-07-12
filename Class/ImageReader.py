38669577c37272ef7c61691bec08ecb2068fd858
Mohammad Heydari <HMohammad2520@Gmail.com>
2024-07-12 09:15:42 +0330

    Enhanced logic

--- Class/ImageReader.py
+++ Class/ImageReader.py
@@ -5,10 +5,8 @@ from PIL import Image as img
 class ImageReader:
     def __init__(self,Tesseract_Path:str = None) -> None :
         self.Tesseract_Path = Tesseract_Path
-        if platform.system().lower() == 'windows' :
+        if platform.system() == 'Windows' :
             pytesseract.tesseract_cmd = self.Tesseract_Path
-        else :
-            print('Make Sure implement teseract with Homebrew')
     
     def Extract_Text (self,Image_Path:str) -> str :
         Image = img.open(Image_Path)
