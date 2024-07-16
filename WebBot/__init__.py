0fd3f06407d90993e9ca3871a19585ce2e18eba3
Mohammad Heydari <HMohammad2520@Gmail.com>
2024-07-16 14:16:54 +0330

    renamed image reader

--- WebBot/__init__.py
+++ WebBot/__init__.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.chrome.service import Service
-from Class.ImageReader import ImageReader
+from Class.image import ImageReader
 
 class WebBot:
     def __init__(self,Driver_Path:str,Base_URL) -> None:
