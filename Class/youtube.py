69cbe55b1935f26411faee8d55df3bfb4ad0c24e
Mohammad Heydari <HMohammad2520@Gmail.com>
2024-07-16 14:14:48 +0330

    Added Yotube downloader repostory as a class

---
+++ Class/youtube.py
@@ -0,0 +1,16 @@
+from pytube import YouTube,Playlist
+
+class YouTubeDownloader :
+    def __init__(self, Name:str) -> None:
+        self.Name = Name
+
+    def Download(self,URL) -> None:
+        try:
+            Download_Instance = Playlist(URL)
+            for Video in Download_Instance.videos:
+                try: Video.streams.filter(file_extension='mp4').order_by('resolution').desc().first().download()
+                except Exception as e: continue
+
+        except KeyError: 
+                Download_Instance = YouTube(URL)
+                Download_Instance.streams.filter(file_extension='mp4').order_by('resolution').desc().first().download()
