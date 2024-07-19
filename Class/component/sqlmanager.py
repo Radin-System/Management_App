949f6ef6050931277586be9b6b2a71386b0872af
Mohammad Heydari <HMohammad2520@Gmail.com>
2024-07-19 19:28:00 +0330

    Shaping SQLManager to look like other classes

--- Class/component/sqlmanager.py
+++ Class/component/sqlmanager.py
@@ -1,7 +1,7 @@
 import os,sqlalchemy,sqlalchemy.orm
 
 from Class.component import Component
-from Function.decorator import Do_Log
+from Function.decorator import Running_Required,Connection_Required,Return_False_On_Exception
 
 class SQLManager(Component):
     def __init__(self,*,
@@ -27,129 +27,96 @@ class SQLManager(Component):
         self.Verbose     = Verbose
         self.Base        = Base
         self.Models      = Models
-        self.Create_Engine()
-        self.Init_Models()
 
-    def Connected(self) -> bool:
-        if self.Engine :
-            try    : self.Engine.connect() ; return True
-            except : return False
-        return False
+    @Running_Required
+    @Return_False_On_Exception
+    def Is_Connected(self) -> bool:
+        self.Engine.connect()
+        return True
 
-    @Do_Log('Creating engine...','Done!')
-    def Create_Engine(self) :
+    @Running_Required
+    def Create_Engine(self):
         if   self.Mode == 'MYSQL'   : self.Engine = sqlalchemy.create_engine(f'mysql+mysqlconnector://{self.Username}:{self.Password}@{self.Host}:{self.Port}/{self.DataBase}',echo=self.Verbose)
         elif self.Mode == 'MSSQL'   : self.Engine = sqlalchemy.create_engine(f'mssql+pyodbc://{self.Username}:{self.Password}@{self.Host}:{self.Port}/{self.DataBase}'        ,echo=self.Verbose)
         elif self.Mode == 'POSTGRE' : self.Engine = sqlalchemy.create_engine(f'postgresql+psycopg2://{self.Username}:{self.Password}@{self.Host}:{self.Port}/{self.DataBase}' ,echo=self.Verbose)
         elif self.Mode == 'MARIADB' : self.Engine = sqlalchemy.create_engine(f'mysql+pymysql://{self.Username}:{self.Password}@{self.Host}:{self.Port}/{self.DataBase}'       ,echo=self.Verbose)
         else                        : self.Engine = sqlalchemy.create_engine(f'sqlite:///{os.path.join(self.SQLite_Path,self.DataBase)}.db'                                   ,echo=self.Verbose)
-        if self.Connected() : self.Base.metadata.create_all(self.Engine)
-        else                : self.Logger('Engine Faild')
-        
-    @Do_Log('Initiating SQL Models...' , 'Initiation Compleated !')
+        self.Base.metadata.create_all(self.Engine)
+
+    @Running_Required
     def Init_Models(self) -> None:
-        for Model in self.Models : 
-            self.Logger(f'Model : {Model.__name__}')
+        for Model in self.Models :
             setattr(self , Model.__name__ , Model)
 
-    def Create(self, Instance) -> int :
-        try:
-            with self.Engine.connect() as Connection:
-                with Connection.begin():
-                    Session = sqlalchemy.orm.Session(bind=Connection)
+    @Running_Required
+    @Connection_Required
+    def Create(self, Instance) -> int:
+        with self.Engine.connect() as Connection:
+            with Connection.begin():
+                with sqlalchemy.orm.Session(bind=Connection) as Session:
                     Session.add(Instance)
                     Session.commit()
-                    Session.close()
-        except Exception as e:
-            self.Logger(f"Error creating record {Instance}")
-            if 'Session' in locals() and Session :
-                Session.rollback()
-                self.Logger("Transaction rolled back.")
-                Session.close()
-            raise e
-        
-    def Update(self, Instance) -> None :
-        try:
-            with self.Engine.connect() as Connection:
-                with Connection.begin():
-                    Session = sqlalchemy.orm.Session(bind=Connection)
+
+    @Running_Required
+    @Connection_Required
+    def Update(self, Instance) -> None:
+        with self.Engine.connect() as Connection:
+            with Connection.begin():
+                with sqlalchemy.orm.Session(bind=Connection) as Session:
                     Session.merge(Instance)
                     Session.commit()
-                    Session.close()
-        except Exception as e : 
-            self.Logger(f"Error updating record {Instance}")
-            if 'Session' in locals() and Session :
-                Session.rollback()
-                self.Logger("Transaction rolled back.")
-                Session.close()
-            raise e
 
-    def Delete(self, Instance) -> None :
-        try:
-            with self.Engine.connect() as Connection:
-                with Connection.begin():
-                    Session = sqlalchemy.orm.Session(bind=Connection)
+    @Running_Required
+    @Connection_Required
+    def Delete(self, Instance) -> None:
+        with self.Engine.connect() as Connection:
+            with Connection.begin():
+                with sqlalchemy.orm.Session(bind=Connection) as Session:
                     Session.delete(Instance)
                     Session.commit()
-                    Session.close()
-        except Exception as e :  
-            self.Logger(f"Error deleting record : {e}")
-            if 'Session' in locals() and Session :
-                Session.rollback()
-                self.Logger("Transaction rolled back.")
-                Session.close()
-            raise e
 
-    def Query(self , 
-                Model , 
-                Eager  : bool = False,
-                Sort   : list = []   ,
-                First  : bool = False, 
-                Limit  : int  = None , 
-                Offset : int  = None , 
-                **Conditions) -> (list | None) : #Usage : SQLManager.Query(SQLManager.User , Eager=True , Sort=[('name','asc')] , First = False , Limit = 10 , Offset = 12 , email = None)
-        try:
-            if not self.Connected() : raise Exception('Not connected to the database.')            
-            with self.Engine.connect() as Connection:
-                with Connection.begin() :
-                    Session = sqlalchemy.orm.Session(bind=Connection)
+    @Running_Required
+    @Connection_Required
+    def Query(self,Model,*, 
+        Eager:bool = False,
+        Sort:list[tuple[str,str]] = [],
+        First:bool = False, 
+        Limit:int = None, 
+        Offset:int = None,
+        **Conditions
+        ) -> (list | None): 
+        #Usage : SQLManager.Query(SQLManager.User , Eager=True , Sort=[('name','asc')] , First = False , Limit = 10 , Offset = 12 , email = None)
+      
+        with self.Engine.connect() as Connection:
+            with Connection.begin():
+                with sqlalchemy.orm.Session(bind=Connection) as Session:
                     Query   = Session.query(Model)
                     if Eager      : Query = Query.options(sqlalchemy.orm.joinedload('*'))
                     if Conditions : Query = Query.filter_by(**Conditions)
                     if Sort       :
                         for attr, order in Sort :
-                            if   order.lower() == 'asc'  : Query = Query.order_by(getattr(Model, attr).asc())
-                            elif order.lower() == 'desc' : Query = Query.order_by(getattr(Model, attr).desc())
-                            else                         : raise ValueError(f"Invalid sorting order: {order}")
-                    if Limit      : Query = Query.limit(Limit)
-                    if Offset     : Query = Query.offset(Offset)
-                    if First      : Result = Query.first()
-                    else          : Result = Query.all()
-                    Session.close()
+                            if   'asc'  in order.lower(): Query = Query.order_by(getattr(Model, attr).asc())
+                            elif 'desc' in order.lower(): Query = Query.order_by(getattr(Model, attr).desc())
+                            else                        : raise ValueError(f"Invalid sorting order: {order}")
+                    if Limit  : Query = Query.limit(Limit)
+                    if Offset : Query = Query.offset(Offset)
+                    if First  : Result = Query.first()
+                    else      : Result = Query.all()
                     return Result
-        except Exception as e :
-            self.Logger(f"Error executing query: {e}")
-            if 'Session' in locals() and Session : Session.close()
-            raise e
 
+    @Running_Required
+    @Connection_Required
     def Count(self, Model, **Conditions) -> int:
-        try:
-            if not self.Connected() : raise Exception('Not connected to the database.')
-            with self.Engine.connect() as Connection :
-                with Connection.begin():
-                    Session = sqlalchemy.orm.Session(bind=Connection)
+        with self.Engine.connect() as Connection:
+            with Connection.begin():
+                with sqlalchemy.orm.Session(bind=Connection) as Session:
                     Query   = Session.query(Model)
                     if Conditions : Query = Query.filter_by(**Conditions)
                     return Query.count()
-        except Exception as e:
-            self.Logger(f"Error executing count query: {e}")
-            if 'Session' in locals() and Session : Session.close()
-            raise e
 
-    @Do_Log('Starting...','Done!')
-    def Start_Actions(self):
-        pass
+    def Start_Actions(self) -> None:
+        self.Create_Engine()
+        self.Init_Models()
     
-    @Do_Log('Stopping...','Done!')
-    def Stop_Actions(self):
+    def Stop_Actions(self) -> None:
         self.Engine = None
