8a616337e6cd79841f7a31f8bbc0ec2203dbdb3b
Mohammad Heydari <HMohammad2520@Gmail.com>
2024-07-20 22:38:33 +0330

    Added Hostname and FQDN

--- Class/validator.py
+++ Class/validator.py
@@ -94,6 +94,36 @@ class Password(Validator):
 
         return True
 
+class Hostname(Validator):
+    def __init__(self, Input: str) -> None:
+        super().__init__(Input)
+
+    @Return_False_On_Exception
+    def Validate(self) -> bool:
+        """
+        Valid hostname onlu contains 16 chars, a-Z , 0-9 and dashes
+        """
+
+        self.Input:str
+
+        if not isinstance(self.Input, str):
+            self.Error_Message = f'Provided input must be a string: {self.Input}'
+            return False
+
+        if len(self.Input) < 2:
+            self.Error_Message = f'Provided hostname is too short: {self.Input}'
+            return False
+
+        if len(self.Input) > 16:
+            self.Error_Message = f'Provided hostname is too long: {self.Input}'
+            return False
+
+        if not re.match(r'^[a-zA-Z0-9-]{1,16}$', self.Input):
+            self.Error_Message = f'Provided hostname can only contain letters, digits, and hyphens: {self.Input}'
+            return False
+
+        return True
+
 class Domain(Validator):
     def __init__(self, Input:str) -> None:
         super().__init__(Input)
@@ -153,6 +183,40 @@ class Domain(Validator):
                 self.Error_Message = f'Provided domain segment only can contain A-Z,a-z,0-9 and "-": {self.Input}'
                 return False
 
+        return True
+
+class FQDN(Validator):
+    def __init__(self, Input:str) -> None:
+        super().__init__(Input)
+
+    @Return_False_On_Exception
+    def Validate(self) -> bool :
+        """
+        FQDN has two parts : Hostname.DomainName
+        """
+
+        self.Input:str
+
+        if not isinstance(self.Input,str) :
+            self.Error_Message = f'Provided email must be a string: {self.Input}'
+            return False
+        
+        if not '.' in self.Input :
+            self.Error_Message = f'Provided FQDN must contain one or more dots: {self.Input}'
+            return False
+
+        Host, Dom = self.Input.split('.',1)
+
+        try: Hostname(Host)
+        except Exception as e :
+            self.Error_Message = f'Hostname Error: {self.Input} : {str(e)}'
+            return False
+        
+        try: Domain(Dom)
+        except Exception as e :
+            self.Error_Message = f'DomainName Error: {self.Input} : {str(e)}'
+            return False
+        
         return True
 
 class Email(Validator):
