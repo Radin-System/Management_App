702027411a20eb668da409a4989ee87a2d0a69f9
Mohammad Heydari <Hmohammad2520@gmail.com>
2024-07-21 12:58:38 +0330

    Removed Whitespaces

--- Class/connection/ssh.py
+++ Class/connection/ssh.py
@@ -10,7 +10,7 @@ class SSH(Connection):
         Username:str,
         Password:str,
         Encoding:str = 'utf-8',
-        ) -> None :
+        ) -> None:
 
         self.Host = Host
         self.Port = Port
@@ -23,41 +23,41 @@ class SSH(Connection):
         self.History = ''
         self.Shell = None
 
-    def Connect(self) -> None :
+    def Connect(self) -> None:
         self.Client.connect(
             hostname = self.Host,
             port     = self.Port,
             username = self.Username,
             password = self.Password,
             )
-        self.Shell  = self.Client.invoke_shell()
+        self.Shell  = self.Client.invoke_shell(width=0,height=0)
         self.Excute_Mode = self.Check_Multi_Channel_Support()
         self.Banner = self.Client.get_transport().remote_version
 
-    def Check_Multi_Channel_Support(self) :
+    def Check_Multi_Channel_Support(self):
         try : 
             self.Client.invoke_shell().close()
             return True
-        except paramiko.ChannelException :
+        except paramiko.ChannelException:
             return False
 
     @Connection_Required
-    def Receive(self) -> str :
+    def Receive(self) -> str:
         if self.Shell.recv_ready():
             return self.Shell.recv(65536).decode(self.Encoding)
 
     @Connection_Required
     def Send(self, Message:str, Wait:float = 0.5) -> str:
         if not Message.endswith('\n'): Message += '\n'
-        if self.Excute_Mode :
+        if self.Excute_Mode:
             _ , Stdout , _ = self.Client.exec_command(Message)
             time.sleep(Wait)
             Response = Stdout.read().decode(self.Encoding)
             self.History += Message
             self.History += Response.replace('\r\n','\n').strip() if Response else ''
             return str(Response).replace('\r\n','\n').strip() if Response else ''
-        else :
-            if not Message.endswith('\n') : Message += '\n'
+        else:
+            if not Message.endswith('\n'): Message += '\n'
             self.Shell.send(Message)
             time.sleep(Wait)
             Response = self.Receive()
