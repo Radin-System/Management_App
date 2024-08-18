7fbe0c347463d2c2849fd4bac04d4d810dfb5b35
Mohammad Heydari <HMohammad2520@Gmail.com>
2024-08-18 19:26:01 +0330

    Simplified DataBase

--- classes/model/__init__.py
+++ classes/model/__init__.py
@@ -1,19 +1,7 @@
-from .tables.authentication import Authentication
-from .tables.company import Company
-#from .tables.device import Device
-from .tables.location import Location
-from .tables.node import Node
-from .tables.personnel import Personnel
 from .tables.user import User
 from ._base import Base
 
 Models = [
-    Authentication,
-    Company,
-#    Device,
-    Location,
-    Node,
-    Personnel,
     User,
     ]
 
