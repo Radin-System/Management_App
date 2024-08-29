b4d781ca2bef816899801e4309d12f213a114889
Mohammad Heydari <Hmohammad2520@gmail.com>
2024-08-29 08:36:57 +0330

    Improvements

--- blueprints/database.py
+++ blueprints/database.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, render_template, request, abort
-from functions.decorator import login_required, admin_required
 
 from classes.component import ComponentContainer, SQLManager
 
@@ -9,12 +8,10 @@ def DataBase(CC: ComponentContainer) -> Blueprint:
     bp = Blueprint('database', __name__, url_prefix='/database')
 
     @bp.route('/')
-    @admin_required
     def index():
         return render_template('database/index.html')
 
     @bp.route('/<Table_Name>', methods=['GET', 'POST', 'PUT', 'DELETE'])
-    @admin_required
     def main(Table_Name):
         Table = SQL.Models.get(Table_Name)
         Action = request.args.get('action', None)
@@ -25,11 +22,10 @@ def DataBase(CC: ComponentContainer) -> Blueprint:
         if Table:
             if request.method == 'GET':
                 if Action == 'list_view':
-                    Data = SQL.Query(Table, Detached=True, Eager=True, Sort=[('id','dsc')])
+                    Data = SQL.Query(Table, Detached=True, Eager=True, Sort=[('id','desc')])
                     return render_template('database/index.html', Table=Table, Data=Data, Action='list_view')
             # Other CRUD operations will get implemented soon
         else:
             return abort(404)
 
     return bp
-
