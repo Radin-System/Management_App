4d86e58d839e438d38f8fe76556ba69d71591f16
Mohammad Heydari <Hmohammad2520@gmail.com>
2024-09-17 12:08:44 +0330

    added Sarv_client

--- classes/tool/webclient/__init__.py
+++ classes/tool/webclient/__init__.py
@@ -1,7 +1,9 @@
 from .netbox_client import NetboxClient
+from .sarv_client import SarvClient
 from .zabbix_client import ZabbixClient
 
 __all__ = [
     'NetboxClient',
+    'SarvClient',
     'ZabbixClient',
 ]
